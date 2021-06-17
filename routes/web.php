a685436e482364438d9df28e1672f4905885bf0c
luisaLuna <luisacasaslu@gmail.com>
2021-06-17 11:31:16 -0500

    getAllNotes route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\NoteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,5 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 
 Route::get("notes/getNotes", [NoteController::class, 'getAllNotes']);
