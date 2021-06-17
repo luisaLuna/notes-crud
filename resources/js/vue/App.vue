c2b1a8c6f752350f0b75cc3d5cf75ddfc66512cb
luisaLuna <luisacasaslu@gmail.com>
2021-06-17 16:38:24 -0500

    Installation of Vue

---
+++ resources/js/vue/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Hello!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  created() {},
+  data() {
+    return {};
+  },
+  props: {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
