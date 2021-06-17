6bccfbc87dbe018d38b827b116d549d1e5af1d7b
luisaLuna <luisacasaslu@gmail.com>
2021-06-17 11:47:30 -0500

    Delete notes route & Disabled CSRF validatation

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::get("notes/getNotes", [NoteController::class, 'getAllNotes']);
+Route::delete("notes/{id}", [NoteController::class, 'delete']); 
